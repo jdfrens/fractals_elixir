16c125a0cb012598ebd0fd90d45cbdf1e4516354
Jeremy D. Frens <jdfrens@gmail.com>
2018-11-30 18:16:29 -0600

    [issue-6] crudest transliteration of tests from png

---
+++ apps/png/test/png_low_level_test.exs
@@ -0,0 +1,138 @@
+defmodule PNG.LowLevelTest do
+  @moduledoc """
+  Transliteration of https://github.com/yuce/png/blob/b6af0face98495f10fba3f638f1f36931ff2b3fe/test/png_low_level_tests.erl
+  """
+
+  use ExUnit.Case, async: true
+
+  # -module(png_low_level_tests).
+
+  # -include_lib("eunit/include/eunit.hrl").
+  # -include_lib("png/include/png.hrl").
+
+  # low_level_test_() ->
+  #     [fun get_header/0,
+  #      fun 'IHDR_width_height'/0,
+  #      fun 'IHDR_width_height_bit_depth'/0,
+  #      fun 'IHDR_width_height_bit_depth_color_type'/0,
+  #      fun compressed_IDAT/0,
+  #      fun raw_IDAT/0,
+  #      fun rows_IDAT/0,
+  #      fun 'PLTE'/0].
+
+  @tag :skip
+  # get_header() ->
+  test "PNG.header/0" do
+    #     Result = png:header(),
+    result = PNG.header()
+    #     Target = <<16#89, 16#50, 16#4E, 16#47, 16#0D, 16#0A, 16#1A, 16#0A>>,
+    target = <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>
+
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  @tag :skip
+  # 'IHDR_width_height'() ->
+  test "IHDR with height" do
+    #     Result = png:chunk('IHDR', #png_config{size = {32, 16}}),
+    result = PNG.chunk("IHDR", PNG.config(size: {32, 16}))
+    #     Target = <<0,0,0,13,73,72,68,82,0,0,0,
+    #                32,0,0,0,16,8,0,0,0,0,82,107,34,133>>,
+    target =
+      <<0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 32, 0, 0, 0, 16, 8, 0, 0, 0, 0, 82, 107, 34, 133>>
+
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  @tag :skip
+  # 'IHDR_width_height_bit_depth'() ->
+  test "IHDR width height bit depth" do
+    #     Result = png:chunk('IHDR', #png_config{size = {32, 16},
+    #                                        mode = ?PNG_GRAYSCALE_8}),
+    result = PNG.chunk("IHDR", PNG.config(size: {32, 26}, mode: PNG_GRAYSCALE_8))
+    #     Target = <<0,0,0,13,73,72,68,82,0,0,0,
+    #                32,0,0,0,16,8,0,0,0,0,82,107,34,133>>,
+    target =
+      <<0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 32, 0, 0, 0, 16, 8, 0, 0, 0, 0, 82, 107, 34, 133>>
+
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  @tag :skip
+  # 'IHDR_width_height_bit_depth_color_type'() ->
+  test "IHDR width height bit depth color type" do
+    #     Result = png:chunk('IHDR', #png_config{size = {32, 16},
+    #                                        mode = ?PNG_RGB_8}),
+    result = PNG.chunk("IHDR", PNG.config(size: {32, 26}, mode: PNG_RGB_8))
+    #     Target = <<0,0,0,13,73,72,68,82,0,0,0,
+    #                32,0,0,0,16,8,2,0,0,0,248,98,234,14>>,
+    target =
+      <<0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 32, 0, 0, 0, 16, 8, 2, 0, 0, 0, 248, 98, 234, 14>>
+
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  @tag :skip
+  # compressed_IDAT() ->
+  test "compressed IDAT" do
+    #     Data = [<<0, 1, 2, 3>>],
+    data = [<<0, 1, 2, 3>>]
+    #     Result = png:chunk('IDAT', {compressed, Data}),
+    result = PNG.chunk("IDAT", {:compressed, data})
+    #     Target = [<<0,0,0,4,73,68,65,84,0,1,2,3,64,222,190,8>>],
+    target = [<<0, 0, 0, 4, 73, 68, 65, 84, 0, 1, 2, 3, 64, 222, 190, 8>>]
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  @tag :skip
+  # raw_IDAT() ->
+  test "raw IDAT" do
+    #     Data = <<0, 1, 2, 3>>,
+    data = <<0, 1, 2, 3>>
+    #     Result = png:chunk('IDAT', {raw, Data}),
+    result = PNG.chunk("IDAT", {:raw, data})
+    #     Target = [<<0,0,0,12,73,68,65,84,120,156,
+    #                 99,96,100,98,6,0,0,14,0,7,215,111,228,120>>],
+    target = [
+      <<0, 0, 0, 12, 73, 68, 65, 84, 120, 156, 99, 96, 100, 98, 6, 0, 0, 14, 0, 7, 215, 111, 228,
+        120>>
+    ]
+
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  @tag :skip
+  # rows_IDAT() ->
+  test "rows IDAT" do
+    #     Data = [<<1, 2, 3>>],
+    data = [<<1, 2, 3>>]
+    #     Result = png:chunk('IDAT', {rows, Data}),
+    result = PNG.chunk("IDAT", {:rows, data})
+    #     Target = [<<0,0,0,12,73,68,65,84,120,156,
+    #                 99,96,100,98,6,0,0,14,0,7,215,111,228,120>>],
+    target = [
+      <<0, 0, 0, 12, 73, 68, 65, 84, 120, 156, 99, 96, 100, 98, 6, 0, 0, 14, 0, 7, 215, 111, 228,
+        120>>
+    ]
+
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  @tag :skip
+  # 'PLTE'() ->
+  test "PLTE" do
+    #     Result = png:chunk('PLTE', {rgb, 8, [{255, 0, 0}, {0, 0, 255}]}),
+    result = PNG.chunk("PLTE", {:rgb, 8, [{255, 0, 0}, {0, 0, 255}]})
+    #     Target = <<0,0,0,6,80,76,84,69,255,0,0,0,0,255,108,161,253,142>>,
+    target = <<0, 0, 0, 6, 80, 76, 84, 69, 255, 0, 0, 0, 0, 255, 108, 161, 253, 142>>
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+end
--- apps/png/test/png_test.exs
+++ apps/png/test/png_test.exs
@@ -1,8 +1,113 @@
 defmodule PNGTest do
-  use ExUnit.Case
-  doctest PNG
+  @moduledoc """
+  Transliteration of https://github.com/yuce/png/blob/b6af0face98495f10fba3f638f1f36931ff2b3fe/test/png_tests.erl
+  """
 
-  test "greets the world" do
-    assert PNG.hello() == :world
+  use ExUnit.Case, async: true
+
+  # -module(png_tests).
+
+  # -include_lib("eunit/include/eunit.hrl").
+  # -include_lib("png/include/png.hrl").
+
+  # iface_test_() ->
+  #     [fun create_append/0].
+
+  # check_config_test_() ->
+  #     [fun check_config/0].
+
+  @tag :skip
+  # create_append() ->
+  test "create_append()" do
+    #     E = ets:new(state, []),
+    e = :ets.new(:state, [])
+
+    #     Cb = fun(Bin) ->
+    #             NewContents = case ets:lookup(E, contents) of
+    #                                 [] ->
+    #                                     [Bin];
+    #                                 [{_, Contents}] ->
+    #                                     [Contents, Bin] end,
+    #         true = ets:insert(E, {contents, list_to_binary(NewContents)}),
+    #         ok end,
+    cb = fn bin ->
+      new_contents =
+        case :ets.lookup(e, :contents) do
+          [] ->
+            [bin]
+
+          [{_, contents}] ->
+            [contents, bin]
+        end
+
+      true = :ets.insert(e, {:contents, list_to_binary(new_contents)})
+      :ok
+    end
+
+    #     Png = png:create(#{size => {4, 2},
+    #                    mode => {indexed, 8},
+    #                    call => Cb,
+    #                    palette => {rgb, 8, [{255, 0, 0}, {0, 0, 255}]}}),
+    png =
+      PNG.create(
+        PNG.config(
+          size: {4, 2},
+          mode: {:indexed, 8},
+          call: cb,
+          palette: {:rgb, 8, [{255, 0, 0}, {0, 0, 255}]}
+        )
+      )
+
+    #     Png = png:append(Png, {rows, [0, 1, 0, 1]}),
+    png = PNG.append(png, {:rows, [0, 1, 0, 1]})
+
+    #     ok = png:close(Png),
+    :ok = PNG.close(png)
+
+    #     [{_, Result}] = ets:lookup(E, contents),
+    [{_, result}] = :ets.lookup(e, :contents)
+
+    #     Target = <<137,80,78,71,13,10,26,10,0,0,0,13,73,72,68,82,0,0,0,4,0,0,0,2,
+    #                8,3,0,0,0,
+    #                72,118,141,81,0,0,0,6,80,76,84,69,255,0,0,0,0,255,108,161,253,
+    #                142,0,0,0,
+    #                14,73,68,65,84,120,156,99,96,96,96,100,0,98,0,0,14,0,3,216,95,
+    #                69,48,0,0,0,0,73,69,78,68,174,66,96,130>>,
+    target =
+      <<137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 4, 0, 0, 0, 2, 8,
+        3, 0, 0, 0, 72, 118, 141, 81, 0, 0, 0, 6, 80, 76, 84, 69, 255, 0, 0, 0, 0, 255, 108, 161,
+        253, 142, 0, 0, 0, 14, 73, 68, 65, 84, 120, 156, 99, 96, 96, 96, 100, 0, 98, 0, 0, 14, 0,
+        3, 216, 95, 69, 48, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130>>
+
+    #     [?_assertEqual(Target, Result)].
+    assert target == result
+  end
+
+  def list_to_binary(io_list) do
+    :erlang.list_to_binary(io_list)
+  end
+
+  @tag :skip
+  # check_config() ->
+  test "check config" do
+    #     Config1 = #png_config{},
+    #     ?assertEqual({error, invalid}, png:check_config(Config1)),
+    config1 = PNG.config()
+    assert {:error, :invalid} = PNG.check_config(config1)
+
+    #     Config2 = #png_config{size = {0, 1}},
+    #     ?assertEqual({error, invalid}, png:check_config(Config2)),
+    config2 = PNG.config(size: {0, 1})
+    assert {:error, :invalid} = PNG.check_config(config2)
+
+    #     Config3 = #png_config{size = {1, 1}},
+    #     ?assertEqual(ok, png:check_config(Config3)),
+    config3 = PNG.config(size: {1, 1})
+    assert :ok = PNG.check_config(config3)
+
+    #     Config4 = #png_config{size = {1, 1}, mode = ?PNG_INDEXED_8},
+    #     ?assertEqual(ok, png:check_config(Config4)).
+    config4 = PNG.config(size: {0, 1}, mode: PNG_INDEXED_8)
+    assert :ok = PNG.check_config(config4)
   end
 end
