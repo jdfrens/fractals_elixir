4f44c4e2628b9d2705f1d8edb28e2152e6177b46
Jeremy D. Frens <jdfrens@gmail.com>
2018-12-11 17:18:26 -0600

    [issue-6] refactor out most of the rest of the png transliteration

--- apps/png/lib/png.ex
+++ apps/png/lib/png.ex
@@ -7,9 +7,9 @@ defmodule PNG do
 
   alias PNG.Config
 
-  @type chunk :: {:compressed, list()}
+  @type chunk ::
+          {:row, iodata()} | {:rows, iodata()} | {:data, iodata()} | {:compressed, iodata()}
 
-  # -spec create(map()) -> map().
   @spec create(map()) :: map()
 
   # create(#{file := File} = Png) ->
@@ -18,106 +18,58 @@ defmodule PNG do
   #     Png2 = maps:remove(file, Png#{call => Callback}),
   #     create(Png2);
 
-  # create(#{size := {Width, Height} = Size,
-  #          mode := Mode,
-  #          call := Callback} = Png) ->
   def create(%{size: {width, height} = size, mode: mode, call: callback} = png) do
-    #     Config = #png_config{size = {Width, Height},
-    #                          mode = Mode},
     config = %Config{size: {width, height}, mode: mode}
-    #     ok = Callback(header()),
     :ok = callback.(header())
-    #     ok = Callback(chunk('IHDR', Config)),
     :ok = callback.(chunk("IHDR", config))
-    #     ok = append_palette(Png),
     :ok = append_palette(png)
-    #     Z = zlib:open(),
     z = :zlib.open()
-    #     ok = zlib:deflateInit(Z),
     :ok = :zlib.deflateInit(z)
-    #     #{size => Size,
-    #       mode => Mode,
-    #       call => Callback,
-    #       z => Z}.
     %{size: size, mode: mode, call: callback, z: z}
   end
 
-  #   append_palette(#{call := Callback, palette := Palette}) ->
   def append_palette(%{call: callback, palette: palette}) do
-    #     Chunk = chunk('PLTE', Palette),
     chunk = chunk("PLTE", palette)
-    #     ok = Callback(Chunk);
     :ok = callback.(chunk)
   end
 
-  #   append_palette(#{}) ->
   def append_palette(%{}) do
-    #     ok.
     :ok
   end
 
-  # -spec append(map(), {row, iodata()} | {rows, iodata()} | {data, iodata()} |
-  #                     {compressed, iodata()}) -> map().
-  @spec append(
-          map(),
-          {:row, iodata()} | {:rows, iodata()} | {:data, iodata()} | {:compressed, iodata()}
-        ) :: map()
-  # append(Png, {row, Row}) ->
+  @spec append(map(), chunk()) :: map()
   def append(png, {:row, row}) do
-    #     append(Png, {data, [0, Row]});
     append(png, {:data, [0, row]})
   end
 
-  # append(Png, {rows, Rows}) ->
   def append(png, {:rows, rows}) do
-    #     F = fun(Row) ->
-    #         [0, Row] end,
     f = fn row -> [0, row] end
-    #     append(Png, {data, lists:map(F, Rows)});
     append(png, {:data, :lists.map(f, rows)})
   end
 
-  # append(#{z := Z} = Png, {data, RawData}) ->
   def append(%{z: z} = png, {:data, raw_data}) do
-    #     Compressed = zlib:deflate(Z, RawData),
     compressed = :zlib.deflate(z, raw_data)
-    #     append(Png, {compressed, Compressed}),
     append(png, {:compressed, compressed})
-    #     Png;
     png
   end
 
-  # append(Png, {compressed, []}) ->
   def append(png, {:compressed, []}) do
-    #     Png;
     png
   end
 
-  # append(#{call := Callback} = Png, {compressed, Compressed}) ->
   def append(%{call: callback} = png, {:compressed, compressed}) do
-    #     Chunks = chunk('IDAT', {compressed, Compressed}),
     chunks = chunk("IDAT", {:compressed, compressed})
-    #     ok = Callback(Chunks),
     :ok = callback.(chunks)
-    #     Png.
     png
   end
 
-  # -spec close(map()) -> ok.
   @spec close(map) :: :ok
-  # close(#{z := Z, call := Callback} = Png) ->
   def close(%{z: z, call: callback} = png) do
-    #     Compressed = zlib:deflate(Z, <<>>, finish),
     compressed = :zlib.deflate(z, <<>>, :finish)
-    #     append(Png, {compressed, Compressed}),
     append(png, {:compressed, compressed})
-    #     ok = zlib:deflateEnd(Z),
     :ok = :zlib.deflateEnd(z)
-    #     ok = zlib:close(Z),
     :ok = :zlib.close(z)
-    #     ok = Callback(chunk('IEND')),
     :ok = callback.(chunk("IEND"))
-    #     ok.
     :ok
   end
 
--- apps/png/test/png_test.exs
+++ apps/png/test/png_test.exs
@@ -5,30 +5,13 @@ defmodule PNGTest do
 
   use ExUnit.Case, async: true
 
-  # -module(png_tests).
-
-  # -include_lib("eunit/include/eunit.hrl").
-  # -include_lib("png/include/png.hrl").
-
-  # iface_test_() ->
-  #     [fun create_append/0].
-
-  # check_config_test_() ->
-  #     [fun check_config/0].
-
-  # create_append() ->
-  test "create_append()" do
-    #     E = ets:new(state, []),
+  # NOTE: the `target` in this test is _not_ the same `Target` in the corresponding test in the original png library.
+  # THE TEST IS BROKEN IN THE ORIGINAL png LIBRARY AND GIVES A FALSE POSITIVE.
+  # I am asserting against the value that the original png library DOES compute (and so does my transliteration).
+  # https://github.com/yuce/png/blob/b6af0face98495f10fba3f638f1f36931ff2b3fe/test/png_tests.erl#L38
+  test "create/1 and append/2" do
     e = :ets.new(:state, [])
 
-    #     Cb = fun(Bin) ->
-    #             NewContents = case ets:lookup(E, contents) of
-    #                                 [] ->
-    #                                     [Bin];
-    #                                 [{_, Contents}] ->
-    #                                     [Contents, Bin] end,
-    #         true = ets:insert(E, {contents, list_to_binary(NewContents)}),
-    #         ok end,
     cb = fn bin ->
       new_contents =
         case :ets.lookup(e, :contents) do
@@ -43,10 +26,6 @@ defmodule PNGTest do
       :ok
     end
 
-    #     Png = png:create(#{size => {4, 2},
-    #                    mode => {indexed, 8},
-    #                    call => Cb,
-    #                    palette => {rgb, 8, [{255, 0, 0}, {0, 0, 255}]}}),
     png =
       PNG.create(%{
         size: {4, 2},
@@ -55,25 +34,11 @@ defmodule PNGTest do
         palette: {:rgb, 8, [{255, 0, 0}, {0, 0, 255}]}
       })
 
-    #     Png = png:append(Png, {rows, [0, 1, 0, 1]}),
     png = PNG.append(png, {:rows, [0, 1, 0, 1]})
 
-    #     ok = png:close(Png),
     :ok = PNG.close(png)
-    #     [{_, Result}] = ets:lookup(E, contents),
     [{_, result}] = :ets.lookup(e, :contents)
 
-    #     Target = <<137,80,78,71,13,10,26,10,0,0,0,13,73,72,68,82,0,0,0,4,0,0,0,2,
-    #                8,3,0,0,0,
-    #                72,118,141,81,0,0,0,6,80,76,84,69,255,0,0,0,0,255,108,161,253,
-    #                142,0,0,0,
-    #                14,73,68,65,84,120,156,99,96,96,96,100,0,98,0,0,14,0,3,216,95,
-    #                69,48,0,0,0,0,73,69,78,68,174,66,96,130>>,
-    # NOTE: this is _not_ the same target as from the original png library.
-    # This IS the same result that the png library gets for this test.
-    # THE TEST IS BROKEN IN THE ORIGINAL png LIBRARY AND GIVES A FALSE POSITIVE.
-    # https://github.com/yuce/png/blob/b6af0face98495f10fba3f638f1f36931ff2b3fe/test/png_tests.erl#L38
-    # I am asserting against the value that the original png library DOES compute (and so does my transliteration).
     target =
       <<137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 4, 0, 0, 0, 2, 8,
         3, 0, 0, 0, 72, 118, 141, 81, 0, 0, 0, 6, 80, 76, 84, 69, 255, 0, 0, 0, 0, 255, 108, 161,
@@ -81,7 +46,6 @@ defmodule PNGTest do
         84, 99, 96, 96, 96, 100, 0, 98, 0, 0, 14, 0, 3, 74, 42, 55, 187, 0, 0, 0, 0, 73, 69, 78,
         68, 174, 66, 96, 130>>
 
-    #     [?_assertEqual(Target, Result)].
     assert target == result
   end
 
