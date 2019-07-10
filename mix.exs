5561a940b14388e39a8219eefe43530d1745c24f
Jeremy D. Frens <jfrens@thecitybase.com>
2019-07-09 19:55:54 -0500

    [issue-48] extract dialyzer() function, switch to term expressions for dialyzer ignore

--- mix.exs
+++ mix.exs
@@ -6,10 +6,7 @@ defmodule Fractals.MixProject do
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      dialyzer: [
-        plt_add_apps: [:ex_unit],
-        ignore_warnings: ".dialyzer-ignore-warnings"
-      ],
+      dialyzer: dialyzer(),
       elixir: "~> 1.8",
       test_coverage: [tool: ExCoveralls],
       aliases: aliases(),
@@ -28,6 +25,13 @@ defmodule Fractals.MixProject do
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
       {:dialyxir, "~> 1.0.0-rc.2", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_add_apps: [:ex_unit],
+      ignore_warnings: ".dialyzer_ignore.exs"
     ]
   end
 
